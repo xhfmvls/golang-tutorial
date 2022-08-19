e611b2815a4e8cf37f21dfce3abe22519048519f
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-08-19 16:57:36 +0700

    named return

--- function.go
+++ function.go
@@ -12,10 +12,20 @@ func getThreeValue() (int, byte, bool) {
 	return 19, 'a', false
 }
 
+func namedGetValue() (a string, b string, c string) {
+	a = "lol"
+	b = "test"
+	c = "pqkt"
+	return c, b, a
+}
+
 func main() {
 	a, b := getTwoValue()
 	fmt.Println(a, b)
 
 	f, _, e := getThreeValue()
 	println(f, e)
+
+	x, y, z := namedGetValue()
+	println(x, y, z)
 }
